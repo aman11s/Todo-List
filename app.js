99da3ec0e378d9b3d33a408c2a39601190810550
Aman Singh <reachout.amansingh@gmail.com>
2021-12-23 02:22:19 +0530

    feat : add todos

---
+++ app.js
@@ -0,0 +1,20 @@
+const addTodosForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const todoHtmlTemplate = (todo) => {
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class='bx bxs-trash delete'></i>
+    </li>`
+    list.innerHTML += html;
+}
+
+addTodosForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addTodosForm.add.value.trim();
+    if(todo.length) {
+        todoHtmlTemplate(todo);
+        addTodosForm.reset();
+    }
+});
